ccc8a16e2dcdd256483d2d0841b5a6691d49d9c9
meno2 <menno.berger@live.nl>
2020-03-10 17:29:42 +0100

    Many a change to data processing. Able to find thrust with these functions, and improved other things.

--- Data_processing/Reference_data_reader.py
+++ Data_processing/Reference_data_reader.py
@@ -1,7 +1,7 @@
 import scipy.io as spio
 import matplotlib.pyplot as plt
 from Cit_par import *
-import numpy as npni
+import numpy as np
 
 #reading the reference data
 'Reference_data.mat'
@@ -62,41 +62,55 @@ This returns an array with the data
 #getting the kets
 #print(reference_data.keys())
 dictlist = reference_data["flightdata"].keys()
-for i in dictlist:
-    print(i,"  ",   reference_data["flightdata"][i]["description"])
-print()
+#for i in dictlist:
+#    print(i,"  ",   reference_data["flightdata"][i]["description"])
+#print()
 
 time_in_secs_utc = reference_data["flightdata"]["Gps_utcSec"]["data"]
 yvalues = reference_data["flightdata"]["Dadc1_alt"]["data"]
 #y2values = reference_data["flightdata"]["Dadc1_alt"]["data"]
 
-TOW = 6689 # Weight for reference data
-
-CL_list = []
-Alpha_list = []
-
-print(reference_data["flightdata"]["lh_engine_FU"]["units"])
-
-for j in range(len(reference_data["flightdata"]["Gps_utcSec"]["data"])):
-    if reference_data["flightdata"]["Gps_utcSec"]["data"][j]>32000 and reference_data["flightdata"]["Gps_utcSec"]["data"][j] < 32500:
-        altitude = reference_data["flightdata"]["Dadc1_alt"]["data"][j]
-        hp0  = altitude *0.3048
-        rho    = rho0 * pow( ((1+(lambd * hp0 / Temp0))), (-((g / (lambd*R)) + 1)))
-        Vel =  reference_data["flightdata"]["Dadc1_tas"]["data"][j] *0.51444
-        Fuel_out_weight = (reference_data["flightdata"]["lh_engine_FU"]["data"][j] +  reference_data["flightdata"]["rh_engine_FU"]["data"][j])*0.453592
-
-        Aircraft_weight = TOW - Fuel_out_weight
-        Aircraft_weight_newton = Aircraft_weight * g
-        CL = 2 * Aircraft_weight_newton / (rho * Vel ** 2 * S)
-        aoa = reference_data["flightdata"]["vane_AOA"]["data"][j]
-        if CL>1:
-            print(altitude, rho, Vel, Fuel_out_weight, CL)
-        CL_list.append(CL)
-        Alpha_list.append(aoa)
 
 
-plt.plot(time_in_secs_utc,yvalues)
-plt.show()
+def cl__values(utcSectime):
+    collecteddata = False
+    TOW = 6689  # Weight for reference data
+    for j in range(len(reference_data["flightdata"]["Gps_utcSec"]["data"])):
+        if int(reference_data["flightdata"]["Gps_utcSec"]["data"][j]) == utcSectime and collecteddata == False:
+            altitude = reference_data["flightdata"]["Dadc1_alt"]["data"][j]
+            hp0 = altitude * 0.3048
+            rho = rho0 * pow(((1 + (lambd * hp0 / Temp0))), (-((g / (lambd * R)) + 1)))
+            Vel = reference_data["flightdata"]["Dadc1_tas"]["data"][j] * 0.51444
+            Fuel_out_weight = (reference_data["flightdata"]["lh_engine_FU"]["data"][j] +
+                               reference_data["flightdata"]["rh_engine_FU"]["data"][j]) * 0.453592
 
-plt.plot(Alpha_list,CL_list)
-plt.show()
+            Aircraft_weight = TOW - Fuel_out_weight
+            Aircraft_weight_newton = Aircraft_weight * g
+            CL = 2 * Aircraft_weight_newton / (rho * Vel ** 2 * S)
+            aoa = reference_data["flightdata"]["vane_AOA"]["data"][j]
+            return CL, aoa
+
+def input_for_thrust_values(utcSectime):
+    collecteddata = False
+    for j in range(len(reference_data["flightdata"]["Gps_utcSec"]["data"])):
+        if int(reference_data["flightdata"]["Gps_utcSec"]["data"][j]) == utcSectime and collecteddata == False:
+            print(reference_data["flightdata"]["time"]["data"][j])
+            altitude  =  reference_data["flightdata"]["Dadc1_alt"]["data"][j] * 0.3048
+            machnum   =  reference_data["flightdata"]["Dadc1_mach"]["data"][j]
+            tempdiff  = reference_data["flightdata"]["Dadc1_tat"]["data"][j] - (Temp0 + lambd * altitude) + 273.15
+            fuelflowL = reference_data["flightdata"]["lh_engine_FMF"]["data"][j] /7936.64
+            fuelflowR = reference_data["flightdata"]["rh_engine_FMF"]["data"][j] /7936.64
+            collecteddata = True
+            print(altitude, machnum, tempdiff, fuelflowL, fuelflowR)
+
+def ref_time_to_utc(min,sec):
+    time = min*60+sec +30832
+    return time
+
+timestamp = ref_time_to_utc(31,59)
+print(timestamp)
+input_for_thrust_values(timestamp)
+
+
+#plt.plot(time_in_secs_utc,yvalues)
+#plt.show()
