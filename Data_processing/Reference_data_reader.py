ecda5558ec47fba93d2d9846c34fea949e74b461
gmmare <gmmare.me@gmail.com>
2020-03-12 16:37:54 +0100

    changed data values to floats

--- Data_processing/Reference_data_reader.py
+++ Data_processing/Reference_data_reader.py
@@ -62,11 +62,11 @@ def get_stat_data(filename, start_line, endline):
     for j in range(start_line-2, endline-2):
         row = []
         for i in range(3,10):
-            value = data_df.iloc[j][i]
+            value = float(data_df.iloc[j][i])
             row.append(value)
         stat_data.append(row)
 
-    return stat_data
+    return np.array(stat_data)
 
 
 reference_data = loadmat('Reference_data.mat')
